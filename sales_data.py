e1a41ecf2b019944abab396f5cb15fd6560efd1f
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-17 22:29:19 +0200

    Added new functions

--- sales_data.py
+++ sales_data.py
@@ -1,2 +1,13 @@
+# sales_data.py
+
 sales_data = [230, 200, 310, 290, 400, 150, 180]
+
+def total_sales(data):
+    return sum(data)
+
+def average_sales(data):
+    return sum(data) / len(data)
+
 print(f"Sales data for the week: {sales_data}")
+print(f"Total sales: {total_sales(sales_data)}")
+print(f"Average sales: {average_sales(sales_data):.2f}")
